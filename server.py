f5eb56ff56a77ef88d0e54d3630420aefc2f2782
BittingBarracuda <kmartinez2397@gmail.com>
2022-11-16 22:16:54 +0100

    Removed LogInThread and ConnectionHandlerThread classes. Replaced with login_user and connection_handler functions.

--- server.py
+++ server.py
@@ -39,50 +39,50 @@ class ChatThread(threading.Thread):
                 lock.release()
                 break
 
-class ConnectionHandlerThread(threading.Thread):
-    def __init__(self):
-        super().__init__()
+# class ConnectionHandlerThread(threading.Thread):
+#     def __init__(self):
+#         super().__init__()
     
-    def run(self):
-        while True:
-            with waiting_condition:
-                lock.acquire()
-                if len(clients) <= 1:
-                    lock.release()
-                    waiting_condition.wait()
-                else:
-                    last_client = clients.pop()
-                    random_client = random.choice(clients)
-                    clients.remove(random_client)
-                    lock.release()
-                    print(f'[+] Chat established between {last_client} && {random_client}')
-                    thread1 = ChatThread(last_client, random_client)
-                    thread2 = ChatThread(random_client, last_client)
-                    thread1.start()
-                    thread2.start()
-
-class LogInThread(threading.Thread):
-    def __init__(self, conn, addr):
-        super().__init__()
-        self.conn = conn
-        self.addr = addr
-    
-    def run(self):
-        # self.conn.send('Type in your new nickname: '.encode('utf-8'))
-        resp = self.conn.recv(1024).decode()
-        lock.acquire()
-        client = Client(self.conn, self.addr, resp)
-        clients.append(client)
-        lock.release()
+def connection_handler():
+    while True:
         with waiting_condition:
-            waiting_condition.notify()
-        print(f'[+] User {client} connected to the server!')
-        self.conn.send(f'[+] Successfully connected to the server!\n[+] Waiting for another user to connect...'.encode('utf-8'))
+            lock.acquire()
+            if len(clients) <= 1:
+                lock.release()
+                waiting_condition.wait()
+            else:
+                last_client = clients.pop()
+                random_client = random.choice(clients)
+                clients.remove(random_client)
+                lock.release()
+                print(f'[+] Chat established between {last_client} && {random_client}')
+                thread1 = ChatThread(last_client, random_client)
+                thread2 = ChatThread(random_client, last_client)
+                thread1.start()
+                thread2.start()
+
+# class LogInThread(threading.Thread):
+#     def __init__(self, conn, addr):
+#         super().__init__()
+#         self.conn = conn
+#         self.addr = addr
+    
+def login_user(conn, addr):
+    # self.conn.send('Type in your new nickname: '.encode('utf-8'))
+    resp = conn.recv(1024).decode()
+    lock.acquire()
+    client = Client(conn, addr, resp)
+    clients.append(client)
+    lock.release()
+    with waiting_condition:
+        waiting_condition.notify()
+    print(f'[+] User {client} connected to the server!')
+    conn.send(f'[+] Successfully connected to the server!\n[+] Waiting for another user to connect...'.encode('utf-8'))
 
 def start_server():
     print(f'[+] Initializing server -> ({SERVER_HOST}, {SERVER_PORT})')
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connection_handler = ConnectionHandlerThread()
+    connection_handler = threading.Thread(target=connection_handler)
     connection_handler.start()
     try:
         server_socket.bind((SERVER_HOST, SERVER_PORT))
@@ -94,7 +94,7 @@ def start_server():
         while True:
             conn, addr = server_socket.accept()
             print('[+] Connection with %s : %s ' %(addr[0], str(addr[1])))
-            thread = LogInThread(conn, addr)
+            thread = threading.Thread(target=login_user, args=(conn, addr))
             thread.start()  
     finally:
         server_socket.close()
